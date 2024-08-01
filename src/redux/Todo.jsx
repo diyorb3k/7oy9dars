2c8895ba50b1d1328723ca42fbbfc27fe9fad0e1
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-08-02 02:41:33 +0500

    daamm

--- src/redux/Todo.jsx
+++ src/redux/Todo.jsx
@@ -1,23 +1,44 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTodo } from "./todoSilcer";
+// import { MdDelete } from "react-icons/md";
+import "./Todo.css";
 
 const Todo = () => {
   const dispatch = useDispatch();
-  const { isLoading, data,error } = useSelector((state) => state.todo);
+  const { isLoading, data, error } = useSelector((state) => state.todo);
   useEffect(() => {
     dispatch(fetchTodo());
   }, [dispatch]);
   console.log(data);
   return (
-    <div>
+    <div className="container">
       {isLoading ? (
         <h1>Loading .....</h1>
       ) : error ? (
-        <h1>error{error}</h1>
+        <h1>error: {error}</h1>
       ) : (
         data.map((todo) => {
-          return <p key={todo.id}>{todo.title}</p>;
+          return (
+            <div key={todo.id}>
+
+              <table>
+                  <tr>
+                    
+                    <td className="id"> {todo.id}</td>
+                    <td className="title">{todo.title}</td>
+                    <td className="completed">{todo.completed}</td>
+                    <td>
+                      <button className="edit">Edit</button>
+                    </td>
+                    <td>
+                      <button className="dalet">Delete</button>
+                    </td>
+                  </tr>
+               
+              </table>
+            </div>
+          );
         })
       )}
     </div>
